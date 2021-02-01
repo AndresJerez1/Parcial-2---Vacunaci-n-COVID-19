951d8630314997c1e590ceaffbf1db93bc6de82b
Andrés Fernando Jerez Medina <andres.jerez.1561@gmail.com>
2021-02-01 03:43:59 +0000

    Falta hacer el while global, permitir actualizar y mostrar la tabla final txt

--- main.cpp
+++ main.cpp
@@ -36,109 +36,6 @@ void leerArchivo()
   };
 
 
-void city1(){
-  ifstream archivo("estadisticas.txt",ios::in);
-    string a;
-    archivo.is_open();
-    string A[5][7];
-
-    for(int i=0;i<5;i++){
-
-      for(int j=0;j<7;j++){
-
-        archivo >> A[i][j];
-        cout<<left<<setw(20)<<A[0][j];
-  
-      }
-    }
-
-    
-};
-
-void city2(){
-  ifstream archivo("estadisticas.txt",ios::in);
-    string a;
-    archivo.is_open();
-    string A[5][7];
-
-    for(int i=0;i<5;i++){
-
-      for(int j=0;j<7;j++){
-
-        archivo >> A[i][j];
-        cout<<left<<setw(20)<<A[1][j];
-  
-      }
-    }
-
-    
-};
-
-void city3(){
-  ifstream archivo("estadisticas.txt",ios::in);
-    string a;
-    archivo.is_open();
-    string A[5][7];
-
-    for(int i=0;i<5;i++){
-
-      for(int j=0;j<7;j++){
-
-        archivo >> A[i][j];
-        cout<<left<<setw(20)<<A[2][j];
-  
-      }
-    }
-
-    
-};
-
-void city4(){
-  ifstream archivo("estadisticas.txt",ios::in);
-    string a;
-    archivo.is_open();
-    string A[5][7];
-
-    for(int i=0;i<5;i++){
-
-      for(int j=0;j<7;j++){
-
-        archivo >> A[i][j];
-        cout<<left<<setw(20)<<A[3][j];
-  
-      }
-    }
-
-    
-};
-
-void city5(){
-  ifstream archivo("estadisticas.txt",ios::in);
-    string a;
-    archivo.is_open();
-    string A[5][7];
-
-    for(int i=0;i<5;i++){
-
-      for(int j=0;j<7;j++){
-
-        archivo >> A[i][j];
-        cout<<left<<setw(20)<<A[4][j];
-  
-      }
-    }
-
-    
-};
-
-
-/*while(!archivo.eof()){
-        archivo >>ciudad>>n_habitantes>>n_contagiados>>n_hospitales>>n_persalud>>n_adultomayor>>distancia;
-        cout<<"Ciudad"<<setw(20)<<"No. Habitantes"<<setw(20)<<"No. Contagiados"<<setw(20)<<"No. Hospitales"<<setw(20)<<"No. Personal salud"<<setw(20)<<"No. Adultos +70"<<setw(20)<<"Distancia (Km)"<<endl;
-        cout<<endl;
-        cout<<ciudad<<setw(10)<<n_habitantes<<setw(20)<<n_contagiados<<setw(20)<<n_hospitales<<setw(20)<<n_persalud<<setw(20)<<distancia;
-        cout<<endl;
-      }*/
 float entrada(){                      //Función usada para las entradas
   float i;
   cin >> i;
@@ -201,6 +98,7 @@ void calc_paradas(){
 
 void orden_tiempo(int cantidad){
   int persalud, mayores, resto;
+  float personal[5],vac_hora_per[5],tiempo[5];
 
   ifstream archivo("estadisticas.txt",ios::in);
   string a;
@@ -286,30 +184,38 @@ void orden_tiempo(int cantidad){
       cout<<"Se vacunarán "<<A[4][4]<<" personas pertenecientes al personal de salud y "<<(cantidad-A[4][4])<<" personas mayores de 70 años"<<endl;
     }else if(cantidad<A[4][4]){
       cout<<"\nEn Medellín: ";
-      cout<<"Se vacunarán "<<cantidad<<" personas pertenecientes al personal de salud"<<endl;
+      cout<<"Se vacunarán "<<cantidad<<" personas pertenecientes al personal de salud\n"<<endl;
     }
 
-    int personal[5],vac_hora_per[5],tiempo[5];
+    cout<<"\nLos siguientes son los tiempos que se gastarán en aplicar todas las vacunas en cada ciudad: \n";
 
-    personal[0]=(0.3)*A[0][4];
-    personal[1]=(0.3)*A[1][4];
-    personal[2]=(0.3)*A[2][4];
-    personal[3]=(0.3)*A[3][4];
-    personal[4]=(0.3)*A[4][4];
+    for(int k=0;k<5;k++){
+      personal[k]=0.3*(A[k][4]);
 
-    vac_hora_per[0]=personal[0]*15;
-    vac_hora_per[1]=personal[1]*15;
-    vac_hora_per[2]=personal[2]*15;
-    vac_hora_per[3]=personal[3]*15;
-    vac_hora_per[4]=personal[4]*15;
+      vac_hora_per[k]=personal[k]*15;
 
-    tiempo[0]=cantidad/vac_hora_per[0];
-    tiempo[1]=cantidad/vac_hora_per[1];
-    tiempo[2]=cantidad/vac_hora_per[2];
-    tiempo[3]=cantidad/vac_hora_per[3];
-    tiempo[4]=cantidad/vac_hora_per[4];
+      tiempo[k]=cantidad/(vac_hora_per[k]);
 
-    cout<<"El tiempo que tardará Barranquilla en aplicar todas las vacunas será: "<<tiempo[0]<<" horas"<<endl;
+    }
+    /*personal[0]=(0.3)*(A[0][4]);
+    personal[1]=(0.3)*(A[1][4]);
+    personal[2]=(0.3)*(A[2][4]);
+    personal[3]=(0.3)*(A[3][4]);
+    personal[4]=(0.3)*(A[4][4]);
+
+    vac_hora_per[0]=(personal[0])*15;
+    vac_hora_per[1]=(personal[1])*15;
+    vac_hora_per[2]=(personal[2])*15;
+    vac_hora_per[3]=(personal[3])*15;
+    vac_hora_per[4]=(personal[4])*15;
+
+    tiempo[0]=cantidad/(vac_hora_per[0]);
+    tiempo[1]=cantidad/(vac_hora_per[1]);
+    tiempo[2]=cantidad/(vac_hora_per[2]);
+    tiempo[3]=cantidad/(vac_hora_per[3]);
+    tiempo[4]=cantidad/(vac_hora_per[4]);*/
+
+    cout<<"\nEl tiempo que tardará Barranquilla en aplicar todas las vacunas será: "<<tiempo[0]<<" horas"<<endl;
     cout<<"El tiempo que tardará Bogotá en aplicar todas las vacunas será: "<<tiempo[1]<<" horas"<<endl;
     cout<<"El tiempo que tardará Bucaramanga en aplicar todas las vacunas será: "<<tiempo[2]<<" horas"<<endl;
     cout<<"El tiempo que tardará Cali en aplicar todas las vacunas será: "<<tiempo[3]<<" horas"<<endl;
@@ -330,86 +236,131 @@ int main() {
 
   leerArchivo();
 
-  
-  /*cout <<"Seleccione la opción que quiere ver "<<endl
-      << "1. Ciudades con más de 600 hospitales:"<< endl
-      << "2. Ciudades con más de 400 Km de didstancia a Bogotá"<<endl;
-  cin >> selector;*/
+  for(int i=0;i<5;i++){
+
+    for(int j=0;j<7;j++){
+
+      archivo >> A[i][j];
+     
+      A[0][1]=1239804;
+      A[1][1]=8380801;
+      A[2][1]=581130;
+      A[3][1]=2496346;
+      A[4][1]=2569007;
+
+      A[0][2]=110000;
+      A[1][2]=611000;
+      A[2][2]=83000;
+      A[3][2]=173000;
+      A[4][2]=325000;
+
+      A[0][3]=724;
+      A[1][3]=2274;
+      A[2][3]=261;
+      A[3][3]=850;
+      A[4][3]=900;
+
+      A[0][4]=90000;
+      A[1][4]=200000;
+      A[2][4]=50000;
+      A[3][4]=100000;
+      A[4][4]=120000;
+
+      A[0][5]=136400;
+      A[1][5]=921800;
+      A[2][5]=63900;
+      A[3][5]=274500;
+      A[4][5]=282500;
+
+      A[0][6]=1000.9;
+      A[1][6]=0;
+      A[2][6]=424.2;
+      A[3][6]=452.4;
+      A[4][6]=416.4;
+
+    }
+  };
+
+  cout<<"Buen día, este programa le ayudará a ver la situación actual del covid 19 en 5 ciudades de Colombia, analizar algunos procesos y ejecutar una simulación de la distribución de un determinado número de vacunas en dichas ciudades. Espero sea de su utilidad el programa, muchas gracias por usarlo"<<endl;
 
 
-  //while(!archivo.eof()){
-    //archivo >>ciudad>>n_habitantes>>n_contagiados>>n_hospitales>>n_persalud>>n_adultomayor>>distancia; //orden de lectura 
+  //*************************MENÚ*****************************************
   
-    /*switch(selector){
+  while(eleccion!=0){
+    cout<<"Seleccione la opción que desea realizar: ";
+    cout<<"\n1.Actualizar número de contagiados";
+    cout<<"\n2.Revisar estadisticas por ciudad";
+    cout<<"\n3.Ingresar número de vacunas y ejecutar el programa\n";
+    eleccion=entrada();
+
+    if(eleccion==0){
+      cout<<"El programa ha finalizado. Gracias por usarlo"<<endl;
+    }
+    if(eleccion < 0 || eleccion >= 4){
+        cout << "Opcion no valida!\nIngrese nuevamente su elección" << endl;
+        eleccion = entrada();
+    }
+  
+    switch(eleccion){
       case 1:
-        if(n_hospitales>600)
-          cout <<left << setw(15)<<ciudad<<left << setw(10)<<n_hospitales <<endl;
+        cout<<"Ingrese el nuevo número de contagiados";
         break;
 
       case 2:
-        if(distancia>400)
-          cout <<left << setw(15)<<ciudad<<left << setw(10)<<distancia <<endl;
+        cout<<"Elija la ciudad que quiere revisar: ";
+        cout<<"\n1. Barranquilla ";
+        cout<<"\n2. Bogotá ";
+        cout<<"\n3. Bucaramanga ";
+        cout<<"\n4. Cali ";
+        cout<<"\n5. Medellín ";
+        opcion=entrada();
+        
+        switch(opcion){
+          case 1:
+          {
+            cout<<"CIUDAD"<<left<<setw(20)<<"# HABITANTES"<<left<<setw(20)<<"# CONTAGIADOS"<<left<<setw(20)<<"# HOSPITALES"<<left<<setw(20)<<"# PERSONAL SALUD"<<left<<setw(20)<<"# A. MAYORES(+70)"<<left<<setw(20)<<"DISTANCIA A BÓGOTA (KM\n)"<<endl;
+            cout<<"Barranquilla"<<left<<setw(20)<<A[0][1]<<left<<setw(20)<<A[0][2]<<left<<setw(20)<<A[0][3]<<left<<setw(20)<<A[0][4]<<left<<setw(20)<<A[0][5]<<left<<setw(25)<<A[0][6];
+            break;
+          }
+          case 2:
+          {
+            cout<<"CIUDAD"<<left<<setw(20)<<"# HABITANTES"<<left<<setw(20)<<"# CONTAGIADOS"<<left<<setw(20)<<"# HOSPITALES"<<left<<setw(20)<<"# PERSONAL SALUD"<<left<<setw(20)<<"# A. MAYORES(+70)"<<left<<setw(20)<<"DISTANCIA A BÓGOTA (KM)\n"<<endl;
+            cout<<"Bogotá"<<left<<setw(20)<<A[1][1]<<left<<setw(20)<<A[1][2]<<left<<setw(20)<<A[1][3]<<left<<setw(20)<<A[1][4]<<left<<setw(20)<<A[1][5]<<left<<setw(25)<<A[1][6];
+            break;
+          }
+          case 3:
+          {
+            cout<<"CIUDAD"<<left<<setw(20)<<"# HABITANTES"<<left<<setw(20)<<"# CONTAGIADOS"<<left<<setw(20)<<"# HOSPITALES"<<left<<setw(20)<<"# PERSONAL SALUD"<<left<<setw(20)<<"# A. MAYORES(+70)"<<left<<setw(20)<<"DISTANCIA A BÓGOTA (KM)\n"<<endl;
+            cout<<"Bucaramanga"<<left<<setw(20)<<A[2][1]<<left<<setw(20)<<A[2][2]<<left<<setw(20)<<A[2][3]<<left<<setw(20)<<A[2][4]<<left<<setw(20)<<A[2][5]<<left<<setw(25)<<A[2][6];
+            break;
+          }
+          case 4:
+          {
+            cout<<"CIUDAD"<<left<<setw(20)<<"# HABITANTES"<<left<<setw(20)<<"# CONTAGIADOS"<<left<<setw(20)<<"# HOSPITALES"<<left<<setw(20)<<"# PERSONAL SALUD"<<left<<setw(20)<<"# A. MAYORES(+70)"<<left<<setw(20)<<"DISTANCIA A BÓGOTA (KM)\n"<<endl;
+            cout<<"Cali"<<left<<setw(20)<<A[3][1]<<left<<setw(20)<<A[3][2]<<left<<setw(20)<<A[3][3]<<left<<setw(20)<<A[3][4]<<left<<setw(20)<<A[3][5]<<left<<setw(25)<<A[3][6];
+            break;
+          }
+          case 5:
+          {
+            cout<<"CIUDAD"<<left<<setw(20)<<"# HABITANTES"<<left<<setw(20)<<"# CONTAGIADOS"<<left<<setw(20)<<"# HOSPITALES"<<left<<setw(20)<<"# PERSONAL SALUD"<<left<<setw(20)<<"# A. MAYORES(+70)"<<left<<setw(20)<<"DISTANCIA A BÓGOTA (KM)\n"<<endl;
+            cout<<"Medellín"<<left<<setw(20)<<A[4][1]<<left<<setw(20)<<A[4][2]<<left<<setw(20)<<A[4][3]<<left<<setw(20)<<A[4][4]<<left<<setw(20)<<A[4][5]<<left<<setw(25)<<A[4][6];
+            break;
+          }
+          default:
+            cout<<"Ingrese un valor válido"<<endl;
+        }
+        break;
+        
+      case 3:
+        cout<<"Ingrese el número de dosis de la vacuna que se van a distribuir \n";
+        x=entrada();
+        calc_dosis_camiones(x);
+        calc_paradas();
+        orden_tiempo(x);
         break;
     }
-
-  };*/
-
-  //MENÚ
-  cout<<"Seleccione la opción que desea realizar: ";
-  cout<<"\n1.Actualizar número de contagiados";
-  cout<<"\n2.Revisar estadisticas por ciudad";
-  cout<<"\n3.Ingresar número de vacunas y ejecutar el programa\n";
-  eleccion=entrada();
-  while(eleccion <= 0 || eleccion >= 4){
-        cout << "Opcion no valida!\nIngrese nuevamente su elección" << endl;
-        eleccion = entrada();
   }
-  
-  
-  switch(eleccion){
-    case 1:
-      cout<<"Ingrese el nuevo número de contagiados";
-    break;
-
-    case 2:
-      cout<<"Elija la ciudad que quiere revisar: ";
-      cout<<"\n1. Barranquilla ";
-      cout<<"\n2. Bogotá ";
-      cout<<"\n3. Bucaramanga ";
-      cout<<"\n4. Cali ";
-      cout<<"\n5. Medellín ";
-      opcion=entrada();
-      
-      switch(opcion){
-        case 1:
-          city1();
-          break;
-        case 2:
-          city2();
-          break;
-        case 3:
-          city3();
-          break;
-        case 4:
-          city4();
-          break;
-        case 5:
-          city5();
-          break;
-      }
-      break;
-
-    case 3:
-      cout<<"Ingrese el número de dosis de la vacuna que se van a distribuir \n";
-      x=entrada();
-      calc_dosis_camiones(x);
-      calc_paradas();
-      orden_tiempo(x);
-
-      break;
-
-
-  }
-  }
+  cout<<"El programa ha finalizado. Gracias por usarlo"<<endl;
+}
 
 
